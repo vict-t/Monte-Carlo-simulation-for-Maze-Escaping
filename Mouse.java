1c80b3529a85bb2d3df2d00cd2ee9122ecb5bb96
vict <44689459+vict-t@users.noreply.github.com>
2022-05-16 16:57:47 +0800

    Create Mouse.java

---
+++ Mouse.java
@@ -0,0 +1,14 @@
+package hw1.s55733192;
+
+import java.util.Random;
+
+/**
+ *
+ * @author mrvictor
+ */
+public class Mouse {
+    double randomDirection() {
+        Random r = new Random();
+        return r.nextDouble();
+    }
+}
