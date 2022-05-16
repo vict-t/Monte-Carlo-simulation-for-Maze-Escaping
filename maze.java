70387b4c02990c3684863a5a0b62d6226d68145e
vict <44689459+vict-t@users.noreply.github.com>
2022-05-16 16:57:15 +0800

    Create maze.java

---
+++ maze.java
@@ -0,0 +1,67 @@
+package maze;
+
+/**
+ *
+ * @author vict-t
+ */
+public class Maze {
+        
+  int maze[][]
+            = {{0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0}};
+
+    public int[][] getMazeArray() {
+        return maze;
+    }
+
+    public void resetMaze() {
+        for (int q = 0; q < 7; q++) {
+            for (int p = 0; p < 7; p++) {
+                maze[q][p] = 0;
+            }
+        }
+    }
+
+    public boolean successEscape(int col, int row) {
+        return (col == 6 && row == 6);
+    }
+    
+    public boolean isVisited(int row, int col) {
+        return maze[row][col] == 0;
+    }
+
+    public boolean isDeadEnd(int row, int col) {
+        boolean left = true, right = true, up = true, down = true;
+        //check if the mouse can go each path one by one
+        if (row >= 6 || maze[row + 1][col] > 0) {
+            left = false;
+        }
+        if (col >= 6 || maze[row][col + 1] > 0) {
+            down = false;
+        }
+        if (row <= 0 || maze[row - 1][col] > 0) {
+            right = false;
+        }
+        if (col <= 0 || maze[row][col - 1] > 0) {
+            up = false;
+        }
+        
+        return (!left && !right && !up && !down);
+    }
+
+    //method for displaying route
+    public void Print() {
+        for (int q = 0; q < 7; q++) {
+            for (int p = 0; p < 7; p++) {
+                System.out.print(maze[q][p] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
